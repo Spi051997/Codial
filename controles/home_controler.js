bc15153e37a7f928a817e0828d660f68565af0e1
Spi051997 <dcodeacedemy97@gmail.com>
2022-03-23 10:16:36 +0530

    Home controller Set-p

---
+++ controles/home_controler.js
@@ -0,0 +1,4 @@
+module.exports.home=(req,res)=>
+{
+   return res.send('<h1>Home page  has lodead<h1>')
+}
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ const express=require('express');
 
 const router=express.Router();
 
+const homecontroller=require('../controles/home_controler');
+
 
 console.log("route is ready")
+
+router.get('/',homecontroller.home)
 
 
 
