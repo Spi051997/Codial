76c827d632285d5700295764c6f56f2b696fd153
Spi051997 <dcodeacedemy97@gmail.com>
2022-03-23 11:45:01 +0530

    View has been set-up

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express=require('express');
+
 const port=8000;
 
 const app=express();
@@ -6,6 +7,14 @@ const app=express();
 // use express
 
 app.use('/',require('./routes'));
+
+
+// set the view enginee
+app.set("view engine","ejs");
+app.set('views','./views')
+
+
+
 
 
 app.listen(port,()=>console.log(`Server is up :${port}`))
