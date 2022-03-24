33c9ccc3081cdc97a353c01ff2f566002e61ac69
Spi051997 <dcodeacedemy97@gmail.com>
2022-03-24 14:30:46 +0530

    added Sign in and Sign_up pages

--- controles/user_controlller.js
+++ controles/user_controlller.js
@@ -4,3 +4,21 @@ module.exports.profile=(req,res)=>
         title:"User"
     })
 }
+
+
+// render Sign_up pages
+module.exports.Signup=(req,res)=>
+{
+    return res.render("user_sign_up",{
+        title:"Sign_up"
+    })
+}
+
+// render Sign_in pages
+
+module.exports.Signin=(req,res)=>
+{
+    return res.render("user_sign_in",{
+        title:"Sign_in"
+    })
+}
--- routes/user.js
+++ routes/user.js
@@ -4,9 +4,20 @@ const userrouter=express.Router();
 
 const userController=require('../controles/user_controlller');
 
+// const Signincontroer=require('../controles/user_controlller')
 
+// for User page
 console.log('user router is ready')
 userrouter.use('/profiles',userController.profile);
+
+//  for user sign in page
+console.log('Sign up is ready');
+userrouter.use('/Signup',userController.Signup);
+
+// for user Sign up  page
+console.log('Sign in  is ready');
+userrouter.use('/Signin',userController.Signin);
+
 
 
 
