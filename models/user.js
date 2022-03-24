0b6ed740ba2fd85ebdedd6073aceb716286f44aa
Spi051997 <dcodeacedemy97@gmail.com>
2022-03-24 14:04:23 +0530

    DB is connected and User schema is created

---
+++ models/user.js
@@ -0,0 +1,29 @@
+// created User scema
+
+const mongoose=require('mongoose');
+
+const userSchema=new mongoose.Schema({
+
+    email:
+    {
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+
+    name:{
+        type:String,
+        required:true
+    }
+
+
+});
+
+
+const usersccr=mongoose.model('user',userSchema);
+
+module.require=usersccr;
