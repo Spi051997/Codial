3b8f24f6ff8b058d4255658adb6296e5b58f28c2
Spi051997 <dcodeacedemy97@gmail.com>
2022-04-01 10:51:17 +0530

    User detai Fetched from DB using poulate func (one to Many relat...

--- controles/home_controler.js
+++ controles/home_controler.js
@@ -5,20 +5,34 @@ module.exports.home=(req,res)=>
     // console.log(req.cookies);
     // res.cookie('us',34);
     
-    Post.find({},(err,posts)=>
+    // Post.find({},(err,posts)=>
+    //     {
+    //         if(err)
+    //         {
+    //             console.log("error in home post");
+    //             return;
+    //         }
+
+    //         return res.render('home',{
+    //             title:'Home',
+    //             posts:posts
+    //         })
+
+    //     })
+
+// using populate  functio to show user detial  on post
+
+    Post.find({}).populate('user').exec((err,posts)=>
+    {
+        if(err)
         {
-            if(err)
-            {
-                console.log("error in home post");
-                return;
-            }
-
-            return res.render('home',{
-                title:'Home',
-                posts:posts
-            })
-
+            console.error('Error while fetching user post data');
+            return;
+        }
+        return res.render('home',{
+            title:'Home',
+            posts:posts
         })
-
+    });
 
 }
