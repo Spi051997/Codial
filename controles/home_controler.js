ac76f7bd7dc1241b068c80c252991201fc7335a8
Spi051997 <dcodeacedemy97@gmail.com>
2022-04-01 10:09:27 +0530

    USer post showing on Home page

--- controles/home_controler.js
+++ controles/home_controler.js
@@ -1,10 +1,24 @@
+const Post=require('../models/post')
 module.exports.home=(req,res)=>
 {
 
-    console.log(req.cookies);
-    res.cookie('us',34);
+    // console.log(req.cookies);
+    // res.cookie('us',34);
+    
+    Post.find({},(err,posts)=>
+        {
+            if(err)
+            {
+                console.log("error in home post");
+                return;
+            }
 
-   return res.render('home',{
-       title:'Home'
-   });
+            return res.render('home',{
+                title:'Home',
+                posts:posts
+            })
+
+        })
+
+
 }
